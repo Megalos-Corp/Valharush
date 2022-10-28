9e3885814322e541ea1be4f82ceadc1d8265b13b
Nesios <52352139+N3siOS@user.noreply.github.com>
2022-10-28 02:58:22 +0200

    update config

--- twitch.js
+++ twitch.js
@@ -1,30 +1,72 @@
+//small
 new Twitch.Embed("twitch-flowmino", {
     width: 583,
     height: 378,
     channel: "FlowMino",
-    layout: "video"
+    layout: "video",
+    allowfullscreen:true
 })
 new Twitch.Embed("twitch-jennyie", {
     width: 583,
     height: 378,
     channel: "jennyie_",
-    layout: "video"
+    layout: "video",
+    allowfullscreen:true
 })
 new Twitch.Embed("twitch-kanumera", {
     width: 583,
     height: 378,
     channel: "Kanumera_",
-    layout: "video"
+    layout: "video",
+    allowfullscreen:true
 })
 new Twitch.Embed("twitch-mogwai", {
     width: 583,
     height: 378,
     channel: "Mogwai_J_NY",
-    layout: "video"
+    layout: "video",
+    allowfullscreen:true
 })
 new Twitch.Embed("twitch-suddiste", {
     width: 583,
     height: 378,
     channel: "suddiste",
-    layout: "video"
+    layout: "video",
+    allowfullscreen:true
+})
+//full
+new Twitch.Embed("twitch-flowmino-full", {
+    width: 1920,
+    height: 1080,
+    channel: "FlowMino",
+    layout: "video",
+    allowfullscreen:true
+})
+new Twitch.Embed("twitch-jennyie-full", {
+    width: 1920,
+    height: 1080,
+    channel: "jennyie_",
+    layout: "video",
+    allowfullscreen:true
+})
+new Twitch.Embed("twitch-kanumera-full", {
+    width: 1920,
+    height: 1080,
+    channel: "Kanumera_",
+    layout: "video",
+    allowfullscreen:true
+})
+new Twitch.Embed("twitch-mogwai-full", {
+    width: 1920,
+    height: 1080,
+    channel: "Mogwai_J_NY",
+    layout: "video",
+    allowfullscreen:true
+})
+new Twitch.Embed("twitch-suddiste-full", {
+    width: 1920,
+    height: 1080,
+    channel: "suddiste",
+    layout: "video",
+    allowfullscreen:true
 })
