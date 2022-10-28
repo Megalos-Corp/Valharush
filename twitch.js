212ab3e93c7e83b0a9f4748f23ebc95db6c20d86
Nesios <52352139+N3siOS@user.noreply.github.com>
2022-10-28 16:17:55 +0200

    re update config

--- twitch.js
+++ twitch.js
@@ -4,35 +4,40 @@ new Twitch.Embed("twitch-flowmino", {
     height: 378,
     channel: "FlowMino",
     layout: "video",
-    allowfullscreen:true
+    allowfullscreen:true,
+    theme:dark
 })
 new Twitch.Embed("twitch-jennyie", {
     width: 583,
     height: 378,
     channel: "jennyie_",
     layout: "video",
-    allowfullscreen:true
+    allowfullscreen:true,
+    theme:dark
 })
 new Twitch.Embed("twitch-kanumera", {
     width: 583,
     height: 378,
     channel: "Kanumera_",
     layout: "video",
-    allowfullscreen:true
+    allowfullscreen:true,
+    theme:dark
 })
 new Twitch.Embed("twitch-mogwai", {
     width: 583,
     height: 378,
     channel: "Mogwai_J_NY",
     layout: "video",
-    allowfullscreen:true
+    allowfullscreen:true,
+    theme:dark
 })
 new Twitch.Embed("twitch-suddiste", {
     width: 583,
     height: 378,
     channel: "suddiste",
     layout: "video",
-    allowfullscreen:true
+    allowfullscreen:true,
+    theme:dark
 })
 //full
 new Twitch.Embed("twitch-flowmino-full", {
@@ -40,33 +45,38 @@ new Twitch.Embed("twitch-flowmino-full", {
     height: 1080,
     channel: "FlowMino",
     layout: "video",
-    allowfullscreen:true
+    allowfullscreen:true,
+    theme:dark
 })
 new Twitch.Embed("twitch-jennyie-full", {
     width: 1920,
     height: 1080,
     channel: "jennyie_",
     layout: "video",
-    allowfullscreen:true
+    allowfullscreen:true,
+    theme:dark
 })
 new Twitch.Embed("twitch-kanumera-full", {
     width: 1920,
     height: 1080,
     channel: "Kanumera_",
     layout: "video",
-    allowfullscreen:true
+    allowfullscreen:true,
+    theme:dark
 })
 new Twitch.Embed("twitch-mogwai-full", {
     width: 1920,
     height: 1080,
     channel: "Mogwai_J_NY",
     layout: "video",
-    allowfullscreen:true
+    allowfullscreen:true,
+    theme:dark
 })
 new Twitch.Embed("twitch-suddiste-full", {
     width: 1920,
     height: 1080,
     channel: "suddiste",
     layout: "video",
-    allowfullscreen:true
+    allowfullscreen:true,
+    theme:dark
 })
