60e583f42263746bf6b74b380eb28d3a8a28d279
Nesios <52352139+N3siOS@user.noreply.github.com>
2022-10-28 02:02:39 +0200

    test multi live overlay

---
+++ twitch.js
@@ -0,0 +1,30 @@
+new Twitch.Embed("twitch-flowmino", {
+    width: 583,
+    height: 378,
+    channel: "FlowMino",
+    layout: "video"
+})
+new Twitch.Embed("twitch-jennyie", {
+    width: 583,
+    height: 378,
+    channel: "jennyie_",
+    layout: "video"
+})
+new Twitch.Embed("twitch-kanumera", {
+    width: 583,
+    height: 378,
+    channel: "Kanumera_",
+    layout: "video"
+})
+new Twitch.Embed("twitch-mogwai", {
+    width: 583,
+    height: 378,
+    channel: "Mogwai_J_NY",
+    layout: "video"
+})
+new Twitch.Embed("twitch-suddiste", {
+    width: 583,
+    height: 378,
+    channel: "suddiste",
+    layout: "video"
+})
