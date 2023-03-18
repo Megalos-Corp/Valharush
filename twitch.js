ec9b31364872b130141ace7f66eb68cbf75d71bf
Nesios <52352139+N3siOS@users.noreply.github.com>
2023-03-18 23:51:41 +0100

    update width and tempate

--- twitch.js
+++ twitch.js
@@ -1,35 +1,42 @@
 //small
 new Twitch.Embed("twitch-flowmino", {
-    width: 583,
-    height: 378,
+    width: 480,
+    height: 270,
     channel: "FlowMino",
     layout: "video",
     allowfullscreen:true
 })
+new Twitch.Embed("twitch-megalosteam", {
+    width: 480,
+    height: 270,
+    channel: "megalosteam",
+    layout: "video",
+    allowfullscreen:true
+})
 new Twitch.Embed("twitch-jennyie", {
-    width: 583,
-    height: 378,
+    width: 480,
+    height: 270,
     channel: "jennyie_",
     layout: "video",
     allowfullscreen:true
 })
 new Twitch.Embed("twitch-kanumera", {
-    width: 583,
-    height: 378,
+    width: 480,
+    height: 270,
     channel: "Kanumera_",
     layout: "video",
     allowfullscreen:true
 })
 new Twitch.Embed("twitch-mogwai", {
-    width: 583,
-    height: 378,
+    width: 480,
+    height: 270,
     channel: "Mogwai_J_NY",
     layout: "video",
     allowfullscreen:true
 })
 new Twitch.Embed("twitch-suddiste", {
-    width: 583,
-    height: 378,
+    width: 480,
+    height: 270,
     channel: "suddiste",
     layout: "video",
     allowfullscreen:true
